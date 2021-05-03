ad25bfc66017d29ca4f3b5ff41b7ddc9ba46f58e
Temo Abesadze <abesadzetemo1997@gmail.com>
2021-05-03 15:06:18 +0400

    started working on backend side, started django project

--- ECOMMERCE-REACT-DJANGO/frontend/src/screens/ProductScreen.js
+++ ECOMMERCE-REACT-DJANGO/frontend/src/screens/ProductScreen.js
@@ -8,10 +8,73 @@ import products from '../products';
 
 
 const ProductScreen = ({ match }) => {
-  const product = products.find(product => product._id == match.params.id)
+  // console.log('match', match);
+
+  const product = products.find(product => product._id === match.params.id);
+
+  // console.log('product', product)
+
   return (
     <div>
-      Product
+      <Link to='/' className='btn btn-light my-3'>Go Back</Link>
+      <Row>
+
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant="flush">
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating value={product.rating} text={`${product.numReviews} reviews`} color={'#f8e825'} />
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Price: ${product.price}
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              Description: {product.description}
+            </ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>{product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>{product.countInStock > 0 ? 'In stock' : 'Out of stock'}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+
+              <ListGroup.Item>
+                <Button className="btn btn-block" type="button" disabled={product.countInStock === 0} >
+                  Add to cart
+                </Button>
+              </ListGroup.Item>
+
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
     </div>
   )
 }
