47b6cd77afc37212e067c8b0e48bbeb8d6efcee5
Temo Abesadze <abesadzetemo1997@gmail.com>
2021-05-03 16:31:32 +0400

    started building the backend, created our own api

--- ECOMMERCE-REACT-DJANGO/backend/backend/settings.py
+++ ECOMMERCE-REACT-DJANGO/backend/backend/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'base.apps.BaseConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
