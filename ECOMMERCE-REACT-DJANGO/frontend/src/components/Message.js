1838873477ed85347cb970c20203b5d4be458794
Temo Abesadze <abesadzetemo1997@gmail.com>
2021-06-04 21:41:38 +0400

    deleted ECOMMERCE-REACT-DJANGO from my projects

--- ECOMMERCE-REACT-DJANGO/frontend/src/components/Message.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Alert } from 'react-bootstrap';
-
-const Message = ({ variant, children }) => <Alert variant={variant}>{children}</Alert>
-
-export default Message;
